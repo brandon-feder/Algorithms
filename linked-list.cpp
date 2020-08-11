67cb82af55ba05ddb873c5b87537fb7038b128d1
Brandon Feder <brandonf@pop-os.localdomain>
2020-08-11 12:42:13 -0400

    Added a few new features to the linked list including searching.

--- linked-list.cpp
+++ linked-list.cpp
@@ -54,8 +54,94 @@ class LinkedList
             return currentNode->value;
         }
 
+        void set(int index, data_type val)
+        {
+            Node* currentNode = root;
+
+            for(int i = 0; i < index; i++)
+            {
+                currentNode = currentNode->next;
+            }
+
+            currentNode->value = val;
+        }
+
         int getSize()
         {
             return size;
+        }
+
+        int remove(int index)
+        {
+            if(index == 0) // If removing the first element
+            {
+                //Set the root to be the second element
+                root = root->next;
+            } else if(index == size-1) /// If removing the last element
+            {
+                // Find the second to last element and set set its next to NULL
+                Node *currentNode = root;
+                for(int i = 0; i < size-2; i++)
+                {
+                    currentNode = currentNode->next;
+                }
+
+                currentNode->next = NULL;
+
+            } else // If removing any element other than the first or last
+            {
+                // Set the index-1 element to point to the index+1 element
+                Node *currentNode = root;
+
+                for(int i = 0; i < index-1; i++)
+                {
+                    currentNode = currentNode->next;
+                }
+
+                currentNode->next = currentNode->next->next;
+            }
+
+            --size;
+            return size;
+        }
+
+        void swap(int a, int b)
+        {
+            int a_val = this->get(a);
+            int temp = a_val;
+            int b_val = this->get(b);
+
+            this->set(a, b_val);
+            this->set(b, temp);
+        }
+
+        // Sorts the list of primatives from smallest to greatest using selection sort
+        void sort()
+        {
+
+            // The node so start the sort on each iteration
+            Node *start = root;
+            for(int i = 0; i < size-1; i++)
+            {
+                Node *currentNode = start->next; // The currentNode being compared
+                Node *minNode = currentNode; // The min node found
+
+                // Find the min
+                for(int j = i+1; j < size; j++)
+                {
+                    if(currentNode->value < minNode->value)
+                    {
+                        minNode = currentNode;
+                    }
+                    currentNode = currentNode->next;
+                }
+
+                // Swap the start and the min
+                data_type temp = start->value;
+                start->value = minNode->value;
+                minNode->value = temp;
+
+                start = start->next; // Increment the element to start on
+            }
         }
 };
--- main.cpp
+++ main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include "linked-list.cpp"
 
+void printList(LinkedList<int> list);
+
 int main( void )
 {
     LinkedList<int> L_List;
 
-    L_List.append(1);
     L_List.append(2);
+    L_List.append(0);
+    L_List.append(6);
+    L_List.append(7);
     L_List.append(3);
-    //
-    std::cout << L_List.get(0) << "\n";
-    std::cout << L_List.get(1) << "\n";
-    std::cout << L_List.get(2) << "\n";
+    L_List.append(8);
+    L_List.append(4);
+    L_List.append(9);
+    L_List.append(1);
+    L_List.append(5);
+
+    printList(L_List);
+
+    L_List.sort();
+
+    std::cout << "==============================\n";
+    printList(L_List);
+}
+
+void printList(LinkedList<int> list) {
+    for(int i = 0; i < list.getSize(); i++) {
+        std::cout << list.get(i) << "\n";
+    }
 }
